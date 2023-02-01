582f6f59b3524d6ffaa94d6c5917b75a8f994b80
Lea Kalmar <lea.kalmar@gmail.com>
2023-02-01 23:40:35 +0100

    feat: set up project

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home-component/home.component';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import { AppRoutingModule } from './app-routing.module';
+import {RouterOutlet} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {HttpClientModule} from "@angular/common/http";
+import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatRadioModule} from "@angular/material/radio";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
+    AppRoutingModule,
+    RouterOutlet,
+    ReactiveFormsModule,
+    MatInputModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatRadioModule
+  ],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}},
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: {duration: 2500}
+    }
+    ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home-component/home.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
+
+@Component({
+  selector: 'app-home-component',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  apiKey: FormControl = new FormControl<string>('');
+  billingCycle: FormControl = new FormControl<any>('ONE_TIME');
+
+  constructor(private http: HttpClient, private router: Router, private _snackBar: MatSnackBar) {
+  }
+
+  onBuy(amount: number, billingCycle: string) {
+    if(this.apiKey.value){
+      this.http.post("http://localhost:9000/auth-service/payment-attempts/", {apiKey: this.apiKey.value ,amount: amount, billingCycle: billingCycle}, { responseType: 'text' as const}).subscribe(response =>{
+        window.location.href = response;
+      })
+    }else{
+      this._snackBar.open("Please fill out your API Key!", "",{duration: 2000})
+    }
+  }
+}
